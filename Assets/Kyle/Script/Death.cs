92de5cc5c2b212d2ae94d0d6e0eddf42f95c59ba
kyle <studio11keyuniverse@gmail.com>
2021-10-14 15:05:34 +1300

    Menu/Rain Model
    Everything in one push

---
+++ Assets/Kyle/Script/Death.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Death : MonoBehaviour
+{
+
+    public void ToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
